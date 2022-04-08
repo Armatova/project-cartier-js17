fb2bfdea425e723e99b8bfe23a98b3aaf6a5ef88
Armatova <armatovaaizusha@gmail.com>
2022-04-08 13:00:33 +0600

    first

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { authContext } from "../../contexts/authContext";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const { currentUser } = useContext(authContext);
+  const NAV_ITEMS = [
+    {
+      title: "JEWELRY & WATCHES",
+      link: "/jewelry&watches",
+      id: 1,
+    },
+
+    {
+      title: "NEWS",
+      link: "/news",
+      id: 3,
+    },
+    {
+      title: "STORES",
+      link: "/stores",
+      id: 4,
+    },
+    {
+      title: "ORDERS",
+      link: "/orders",
+      id: 5,
+    },
+  ];
+  const location = useLocation();
+  return (
+    <div className="navbar" style={{ textDecoration: "none" }}>
+      {NAV_ITEMS.map((item) => (
+        <Link
+          style={{ textDecoration: "none" }}
+          className={
+            location.pathname === item.link
+              ? "navbar-item-active"
+              : "navbar-item"
+          }
+          key={item.id}
+          to={item.link}
+        >
+          {item.title}
+        </Link>
+      ))}
+      {currentUser ? (
+        <Link
+          style={{ textDecoration: "none" }}
+          to="/admin"
+          className={
+            location.pathname === "/admin"
+              ? "navbar-item-active"
+              : "navbar-item"
+          }
+        >
+          ADMIN
+        </Link>
+      ) : null}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/helpers/consts.js
@@ -0,0 +1,9 @@
+export const JEWELRY_API = "http://localhost:8001/jewelry";
+
+export const ADMIN_EMAIL = "js17@gmail.com";
+
+//admin password: Chikaaizada22
+
+// user down below
+// js1717@gmail.com
+// Js123123
